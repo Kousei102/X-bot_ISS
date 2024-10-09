6accc222675b27bd636d3889801f0ac7d45e74c8
Kousei102 <130143085+Kousei102@users.noreply.github.com>
2024-10-10 00:35:42 +0900

    Update tweetscript.py

--- tweetscript.py
+++ tweetscript.py
@@ -2,6 +2,7 @@ print("Hello World")
 
 import os
 import tweepy
+from datetime import datetime
 
 #secretsで設定した値をとる
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
@@ -17,6 +18,10 @@ client = tweepy.Client(
   access_token_secret = ACCESS_TOKEN_SECRET
 )
 
+# 現在時刻を取得
+nowtime = datetime.now()
+formatted_time = nowtime.strftime("%Y-%m-%d %H:%M:%S")
+
 import requests
 
 response = requests.get('http://api.open-notify.org/iss-now.json')
@@ -25,7 +30,7 @@ print(f"緯度：{data['iss_position']['latitude']}")
 print(f"軽度：{data['iss_position']['longitude']}")
 
 # ツイート内容を作成
-tweet_text = f"ISSの現在の位置\n緯度：{data['iss_position']['latitude']}\n経度：{data['iss_position']['longitude']}"
+tweet_text = f"現在時刻：formatted_time\nISSの現在の位置\n緯度：{data['iss_position']['latitude']}\n経度：{data['iss_position']['longitude']}"
 
 #ツイートする
 client.create_tweet(text=tweet_text)
