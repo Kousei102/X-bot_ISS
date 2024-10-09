21b6cb87e1e02a8220ec731361a4830de6625127
Kousei102 <130143085+Kousei102@users.noreply.github.com>
2024-10-10 00:58:06 +0900

    Update tweetscript.py

--- tweetscript.py
+++ tweetscript.py
@@ -1,34 +1,47 @@
 import os
 import tweepy
 from datetime import datetime
+import requests
 
-#secretsで設定した値をとる
+# secretsで設定した値をとる
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+OPENCAGE_API_KEY = os.environ['OPENCAGE_API_KEY']  # OpenCageのAPIキー
 
-#オブジェクト作成
+# オブジェクト作成
 client = tweepy.Client(
-  consumer_key = CONSUMER_KEY,
-  consumer_secret = CONSUMER_SECRET,
-  access_token = ACCESS_TOKEN,
-  access_token_secret = ACCESS_TOKEN_SECRET
+    consumer_key = CONSUMER_KEY,
+    consumer_secret = CONSUMER_SECRET,
+    access_token = ACCESS_TOKEN,
+    access_token_secret = ACCESS_TOKEN_SECRET
 )
 
 # 現在時刻を取得
 nowtime = datetime.now()
-formatted_time = nowtime.strftime("%H:%M:%S")
+formatted_time = nowtime.strftime("%Y-%m-%d %H:%M:%S")
 
-import requests
-
+# ISSの位置情報を取得
 response = requests.get('http://api.open-notify.org/iss-now.json')
 data = response.json()
-print(f"緯度：{data['iss_position']['latitude']}")
-print(f"軽度：{data['iss_position']['longitude']}")
+latitude = data['iss_position']['latitude']
+longitude = data['iss_position']['longitude']
+
+# 逆ジオコーディングを使って場所の名前を取得
+geocoding_url = f"https://api.opencagedata.com/geocode/v1/json?q={latitude}+{longitude}&key={OPENCAGE_API_KEY}"
+geo_response = requests.get(geocoding_url)
+geo_data = geo_response.json()
+
+if geo_data['results']:
+    location = geo_data['results'][0]['formatted']  # 最初の結果から場所の名前を取得
+else:
+    location = "海の上"
 
 # ツイート内容を作成
-tweet_text = f"時刻: {formatted_time}\nISSの現在の位置\n緯度： {data['iss_position']['latitude']}\n経度： {data['iss_position']['longitude']}"
+tweet_text = f"時刻: {formatted_time}の時点でのISSの位置\n緯度: {latitude}, 経度: {longitude}\n場所: {location}の付近です"
 
-#ツイートする
+# ツイートを送信
 client.create_tweet(text=tweet_text)
+
+print("ツイートが送信されました！")
